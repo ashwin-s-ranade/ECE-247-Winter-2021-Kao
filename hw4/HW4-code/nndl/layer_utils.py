f3594743f07fc4e23d9aa80a29c66729808ee2aa
Ashwin Ranade <ashwinranade99@gmail.com>
2021-02-10 16:07:19 -0800

    Add hws1-4, 4 not completedl yet

---
+++ hw4/HW4-code/nndl/layer_utils.py
@@ -0,0 +1,62 @@
+from .layers import *
+
+""" 
+This code was originally written for CS 231n at Stanford University
+(cs231n.stanford.edu).  It has been modified in various areas for use in the
+ECE 239AS class at UCLA.  This includes the descriptions of what code to
+implement as well as some slight potential changes in variable names to be
+consistent with class nomenclature.  We thank Justin Johnson & Serena Yeung for
+permission to use this code.  To see the original version, please visit
+cs231n.stanford.edu.  
+"""
+
+def affine_relu_forward(x, w, b):
+  """
+  Convenience layer that performs an affine transform followed by a ReLU
+
+  Inputs:
+  - x: Input to the affine layer
+  - w, b: Weights for the affine layer
+
+  Returns a tuple of:
+  - out: Output from the ReLU
+  - cache: Object to give to the backward pass
+  """
+  a, fc_cache = affine_forward(x, w, b)
+  out, relu_cache = relu_forward(a)
+  cache = (fc_cache, relu_cache)
+  return out, cache
+
+
+def affine_relu_backward(dout, cache):
+  """
+  Backward pass for the affine-relu convenience layer
+  """
+  fc_cache, relu_cache = cache
+  da = relu_backward(dout, relu_cache)
+  dx, dw, db = affine_backward(da, fc_cache)
+  return dx, dw, db
+
+
+def affine_batchnorm_forward(x, w, b, gamma, beta, alpha): #batchnorm parameter -> alpha  
+    
+    affine_out, affine_cache = affine_forward(x, w, b)
+    batchnorm_out, batchnorm_cache = batchnorm_forward(affine_out, gamma, beta, alpha) 
+    
+    y, rcache = relu_forward(batchnorm_out) #y is what we return, also y = gamma*x + beta
+    
+    #rewrite cache 
+    cache = affine_cache, batchnorm_cache, rcache 
+    
+    return y, cache 
+
+def affine_batchnorm_backward(dout, cache): 
+    
+    #following the affine_relu_forward and affine_relu_backward examples 
+    affine_cache, batchnorm_cache, rcache = cache 
+    da = relu_backward(dout, rcache) 
+    
+    daff, dgam, dbet = batchnorm_backward(da, batchnorm_cache) 
+    dx, dw, db = affine_backward(daff, affine_cache) 
+    
+    return dx, dw, db, dgam, dbet 
